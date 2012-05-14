bc6dbde51be1d5752ae35a3a64ec9a3c6b5aba64
Julien Brun <jb007270@hotmail.fr>
2012-05-14 22:54:46 +0200

    Ajout de donnee et correction css

--- config.php
+++ config.php
@@ -10,10 +10,12 @@
 	$allRight = array('add_event' => true, 'edit_event'=>true, 'delete_event'=>true, 'print_listing'=>true, 'add_user'=>true, 'edit_user'=>true, 'delete_user'=>true, 'manage_rights'=>true);
 	$rights = array($allRight, $allRight, $allRight);
 		
-	$event1 = array('id'=>0, 'name' => 'La fête de la patate', 'description' => 'C\'est le jour où toute des patates de retrouve et dansent jusqu\'au bout de la nuit !', 'placeLibre' => '12', 'creator' => $user4, 'admin' =>  array( $user1, $user3));
-	$event2 = array('id'=>1, 'name' => 'Résolution dynamique du lien fort', 'description' => 'Gestion d\'un lien fort entre deux base de donnée avec mise a jour dynamique et gestion des exeptions', 'placeLibre' => '274', 'creator' => $user1, 'admin' =>  array($user4));
+	$event0 = array('id'=>0, 'name' => 'La fête de la patate', 'description' => 'C\'est le jour où toute des patates de retrouve et dansent jusqu\'au bout de la nuit !', 'placeLibre' => '12', 'creator' => $user4, 'admin' =>  array( $user1, $user3));
+	$event1 = array('id'=>1, 'name' => 'Résolution dynamique du lien fort', 'description' => 'Gestion d\'un lien fort entre deux base de donnée avec mise a jour dynamique et gestion des exeptions', 'placeLibre' => '274', 'creator' => $user1, 'admin' =>  array($user4));
+	$event2 = array('id'=>2, 'name' => 'Parallélisme dans les clusters de calcule virtuels', 'description' => 'Etude les mouvements de données dans les clusters de calcule virtualisé a l\'aire d\'un agent hyperviseur', 'placeLibre' => '1045', 'creator' => $user1, 'admin' =>  array($user4));
+	$event3 = array('id'=>3, 'name' => 'Exploration d\'espace hostils par robots autonome', 'description' => 'Programation de robots permetant de catographier en toute autonomie des milieux hostils. Récuperation des données sur grande distance (~100 km) via une technologie sans-fil', 'placeLibre' => '5', 'creator' => $user1, 'admin' =>  array($user4));
 	
-	$events = array($event1, $event2);
+	$events = array($event0, $event1, $event2, $event3);
 	
 	$registered_count = 69;
 	
