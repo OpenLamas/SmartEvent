dd19967d5fd4ddeafd5b0760b2e751b3787fe718
maxmouchet <mouchet.max@gmail.com>
2012-05-14 19:41:39 +0200

    (#2) Suppression de sample.twig.html

--- config.php
+++ config.php
@@ -1,11 +1,19 @@
 <?php
-	$user = array(	array('prenom' => 'Jean', 'nom'=> 'Moulin', 'login' => 'jeanMoul'),
-			array('prenom' => 'Pierre', 'nom'=> 'Quiroule', 'login' => 'pierreQuir'),
-			array('prenom' => 'Mathieu', 'nom'=> 'De chateau', 'login' => 'mathDechate')
+	$user = array(	array('firstname' => 'Jean', 'lastname'=> 'Moulin', 'login' => 'jeanMoul'),
+			array('firstname' => 'Pierre', 'lastname'=> 'Quiroule', 'login' => 'pierreQuir'),
+			array('firstname' => 'Mathieu', 'lastname'=> 'De chateau', 'login' => 'mathDechate')
 		);
 
-	$right = array(	array('add_event' => true, 'edit_event'=>true, 'delete_event'=>true, 'print_listing'=>true),
+	$rights = array(	array('add_event' => true, 'edit_event'=>true, 'delete_event'=>true, 'print_listing'=>true),
 			array('add_event' => true, 'edit_event'=>true, 'delete_event'=>true, 'print_listing'=>true),
 			array('add_event' => true, 'edit_event'=>true, 'delete_event'=>true, 'print_listing'=>true)
 		);
+		
+	$event = array( array('name' => 'La fête de la patate', 'description' => 'C\'est le jour où toute des patates de retrouve et dansent jusqu\'au bout de la nuit !', 'placeLibre' => '12'),
+	array('name' => 'Résolution dynamique du lien fort', 'description' => 'Gestion d\'un lien fort entre deux base de donnée avec mise a jour dynamique et gestion des exeptions', 'placeLibre' => '274')
+				);
+				
+	$events = array( 'registered_count' => 2);
+	
+	
  ?>
