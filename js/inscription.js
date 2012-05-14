2f7b3c1555f302e037c23b8cae40f5b6e40ccb3a
Julien Brun <jb007270@hotmail.fr>
2012-05-14 23:51:17 +0200

    Ajout desinscription et animation

--- js/inscription.js
+++ js/inscription.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 	$('.modal .modal-footer .inscrire').click(function(e) {
-		$(this).html("Inscrit");
+		if(!$(this).hasClass("inscrit")){
+			$(this).html('<span style="display: none;">Inscrit</span>');
+			$('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>Se désinscrire</span>');});
+			$(this).addClass("inscrit");
+			}
+		
+		else{
+			$(this).html('<span style="display: none;">Désinscrit</span>');
+			$('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>S\'inscrire</span>');});
+			$(this).removeClass("inscrit");
+		}
 		e.preventDefault();
 	});
 });
