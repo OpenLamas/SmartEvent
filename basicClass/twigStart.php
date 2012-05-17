18ad36094b2f69384cbf4a4bae9575f41ee9697f
Julien Brun <jb007270@hotmail.fr>
2012-05-17 17:36:59 +0200

    Refactorisation de code ajout controleur et model

---
+++ basicClass/twigStart.php
@@ -0,0 +1,22 @@
+<?php 
+	require_once('/../Twig/Autoloader.php');
+
+	class Controller{
+		protected $twig;
+		protected $view_folder;
+		protected $loader;
+		
+		public function __construct(){
+			Twig_Autoloader::register();
+			
+			// $nb_u = (isset($_GET['user']) && !empty($_GET['user']) ? intval($_GET['user']) : '1');
+		
+			// Config Twig
+			$this->view_folder = 'views'; // Dossier contenant les templates
+			$this->loader = new Twig_Loader_Filesystem($this->view_folder); 
+			$this->twig = new Twig_Environment($this->loader, array(
+				'cache' => false, 'debug' => true, 'strict_variables' => true
+			));
+		}
+	}
+?>
---
+++ controller/class_event.php
@@ -0,0 +1,28 @@
+<?php
+  require('/../dao/class_db_request.php');
+	require('/../basicClass/twigStart.php');
+	
+	class Home extends Controller{
+		
+		public function listEvents(){
+			$donnees = new db_request();
+			
+			$template = $this->twig->loadTemplate('home.twig');
+			echo $template->render(array('user' => $donnees->getUser(1), 'rights' => $donnees->getRights(1), 'events' => $donnees->getAllEvents(), 'registered_count' => $donnees->getRegisteredCount()));
+		}
+		
+		public function detail($id){
+			
+			if(isset($_GET['ajax'])){
+				$addClass = 'modal fade';
+			}
+			else{
+				$addClass = 'nojavascript';
+			}
+			
+			$donnees = new db_request();
+			$template = $this->twig->loadTemplate('detail.twig');
+			echo $template->render('user' => $user[$nb_u], 'rights' => $donnees->getRights(), 'event' => $donnees->getEvent($id), 'get' => $addClass));
+		}
+	}
+?>
