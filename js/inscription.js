88ec482e3fb6f2f8608d77d9ad27da192f72f442
Julien Brun <jb007270@hotmail.fr>
2012-05-19 15:22:59 +0200

    Rcuperation des modals via Ajax

--- js/inscription.js
+++ js/inscription.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
-	$('.modal .modal-footer .inscrire').click(function(e) {
-		if(!$(this).hasClass("inscrit")){
-			$(this).html('<span style="display: none;">Inscrit</span>');
-			$('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>Se désinscrire</span>');});
-			$(this).addClass("inscrit");
-			}
-		
-		else{
-			$(this).html('<span style="display: none;">Désinscrit</span>');
-			$('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>S\'inscrire</span>');});
-			$(this).removeClass("inscrit");
-		}
-		e.preventDefault();
-	});
+  $('.modal .modal-footer .inscrire').click(function(e) {
+    if(!$(this).hasClass("inscrit")){
+      $(this).html('<span style="display: none;">Inscrit</span>');
+      $('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>Se désinscrire</span>');});
+      $(this).addClass("inscrit");
+      }
+    
+    else{
+      $(this).html('<span style="display: none;">Désinscrit</span>');
+      $('span',this).fadeIn(1000, function() {$('.modal .modal-footer .inscrire').html('<span>S\'inscrire</span>');});
+      $(this).removeClass("inscrit");
+    }
+    e.preventDefault();
+  });
   
-  /*$('.detailBtn').click(function (e) {
+  $('.detailBtn').click(function (e) {
     var href = $(this).attr("href");
-    alert(href);
-    $('.ajaxCont').load( href + '.detail', function() {
-      
+    var eventIdArray = href.split('-');
+    eventId = eventIdArray[1];
+    var indexA = $('.detailBtn').index(this);
+    $('.ajaxCont:eq('+indexA+')').load('http://localhost/projet-ic4/'+href+'-ajax .detail', function() {
+        $('#myModal'+eventId).modal('show');
       });
     
     e.preventDefault();
   });
-});*/
+});
