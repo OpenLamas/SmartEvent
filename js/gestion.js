c0ca29125dcee8c7d6492ef679fb6224e269e5f8
Julien Brun <jb007270@hotmail.fr>
2012-05-21 18:53:52 +0200

    Fix #16

--- js/gestion.js
+++ js/gestion.js
@@ -5,6 +5,26 @@ $(document).ready(function () {
   }); 
   
   var idUsers = new Array();
+
+  // Selection global
+
+  $('#utilisateurs thead input').click(function(){
+    if(!$(this).attr('checked')){ //on lit l'event après le clique donc on inverse les états
+      idUsers.splice(0, idUsers.length);
+      $('#utilisateurs tbody tr').each(function(){
+        $('input', this).attr("checked", false);
+      });
+    }
+
+    else{
+      $('#utilisateurs tbody input').each(function(){
+        idUsers.push($(this).attr("name"));
+        $('input').attr("checked", true);
+      });
+    }
+  })
+
+  // Selection des lignes
   $('#utilisateurs tbody tr').click(function() {
     if($('input', this).attr('checked')){
       $('input', this).attr('checked', false);
@@ -18,6 +38,20 @@ $(document).ready(function () {
       idUsers.push(tmp[1]);
     }
   });
+
+  /*// Rétablissement des events sur les case a coché
+
+  $('#utilisateurs tbody tr input').click(function() {
+    if($(this).attr('checked')){
+      idUsers.splice(idUsers.indexOf($(this).attr('name')),1);
+      $('input', this).attr('checked', false);
+    }
+
+    else{
+      idUsers.push($(this).attr('name'));
+      $('input', this).attr('checked', true);
+    }
+  });*/
   
   $('#utilisateurs .btn-danger').click(function(){
     if(idUsers.length > 0){
