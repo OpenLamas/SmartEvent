a529bc4e289f90fd2020fa442d9a290fb09e0bdd
root <root@debian.(none)>
2012-05-21 15:53:11 +0200

    fix login page

--- controller/user/class_Login.php
+++ controller/user/class_Login.php
@@ -1,30 +1,28 @@
 <?php
   require('dao/class_db_request.php');
-	require('basicClass/twigStart.php');
-	
-	class Login extends Controller{
-		
-		public function action(){
-			$state = '';
+  require('basicClass/twigStart.php');
+
+  class Login extends Controller{
+
+    public function action(){
+      $state = '';
       if($_SERVER['REQUEST_METHOD'] == 'POST'){
-				$donnees = new db_request();
-        
+        $donnees = new db_request();
         if(isset($_POST['email']) && isset($_POST['password'])){
-					$tmpUser = $donnees->getPassword(addslashes($_POST['email']));
-          if($tmpUser['password'] == md5(addslashes($_POST['password'])));{
+          $tmpUser = $donnees->getPassword(addslashes($_POST['email']));
+          if($tmpUser['password'] == md5(addslashes($_POST['password']))){
             $_SESSION = $donnees->getUser($tmpUser['id']);
-            
             $state = 'success';
           }
           $state = 'wrong';
-				}
+        }
         $state = 'noData';
-			}
-			$template = $this->twig->loadTemplate('login.twig');
+      }
+      $template = $this->twig->loadTemplate('login.twig');
       if(!isset($_SESSION['login'])){
         $_SESSION['login'] = '';
       }
-			echo $template->render(array('cur_user' => $_SESSION, 'state' => $state));
-		}
-	}
+      echo $template->render(array('cur_user' => $_SESSION, 'state' => $state));
+    }
+  }
 ?>
