823afad93edb55198302e09f0d0b2c80fbd6a6f6
Julien Brun <jb007270@hotmail.fr>
2012-06-12 23:17:23 +0200

    Clean js

--- app/js/inscription.js
+++ app/js/inscription.js
@@ -1,26 +1,3 @@
-(function($) {
-
-  $.fn.konami = function(callback, code) {
-    if(code == undefined) code = "38,38,40,40,37,39,37,39,66,65";
-    
-    return this.each(function() {
-      var kkeys = [];
-      $(this).keydown(function(e){
-        kkeys.push( e.keyCode );
-        while (kkeys.length > code.split(',').length) {
-          kkeys.shift();
-        }
-        if ( kkeys.toString().indexOf( code ) >= 0 ){
-          $(this).unbind('keydown', arguments.callee);
-          callback(e);
-        }
-      });
-});
-}
-
-})(jQuery);
-
-
 $(document).ready(function() {
   
   $.datepicker.regional['fr'] = {
@@ -45,9 +22,4 @@ $(document).ready(function() {
   $.datepicker.setDefaults($.datepicker.regional['fr']);
 
   $('.date').datepicker();
-
-  $(window).konami(function(){ 
-    $('body').append('<div class="modal fade" id="konamiMod"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">×</button><h3>Good job /b/ro !</h3></div><div class="modal-body"><p>Bien joué ! Tu a trouvé un Easter Egg !<br />Va-tu trouver les autres....<br />Surtout, ne dit a personne que tu est venu ici. Ca restera notre petit secret ;)</p></div><div class="modal-footer"><a href="#" class="btn" data-dismiss="modal">Fermer</a></div></div>');
-    $('#konamiMod').modal("show");
-  });
 });
