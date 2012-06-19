37fb85a0d421b20c26fdc21b6eaf2a71d38e548b
maxmouchet <mouchet.max@gmail.com>
2012-06-19 13:04:20 +0200

    SendOneMail() working

--- app/basicClass/mailwrapper.php
+++ app/basicClass/mailwrapper.php
@@ -8,16 +8,16 @@
     }
 
     public function SendOneMail($from, $to, $subject, $message) {
-      $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'tls')
-        ->setUsername('mouchet.max@gmail.com')
-        ->setPassword("@v9#\$bb25x5h'7dmseg@\\n");
+      $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+        ->setUsername('...')
+        ->setPassword("...");
 
       $mailer = Swift_Mailer::newInstance($transport);
 
       $message = Swift_Message::newInstance()
         ->setSubject($subject)
-        ->setFrom(array('mouchet.max@gmail.com' => 'Maxime Mouchet'))
-        ->setTo(array("$to" => "aa"))
+        ->setFrom(array("$from" => "$from"))
+        ->setTo(array("$to" => "$to"))
         ->setBody($message);
 
       echo var_dump($transport);
