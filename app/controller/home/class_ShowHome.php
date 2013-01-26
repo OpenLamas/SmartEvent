4df37b628c7bdc79320591ce639f6b313d8443d9
Maxime Mouchet <mouchet.max@gmail.com>
2013-01-27 00:29:19 +0100

    Refactorisation de home

--- app/controller/home/class_ShowHome.php
+++ app/controller/home/class_ShowHome.php
@@ -1,30 +1,27 @@
 <?php
   require('dao/class_db_request.php');
   require('basicClass/twigStart.php');
-  
+  require('helpers/helpers.php');
+
   class ShowHome extends Controller{
 
     public function action(){
 
+      redirectIfNotLogged();
+      redirectIfHasNotTheRight('UTILISATEUR');
+
       $dbEvents = new db_events();
       $dbSessions = new db_sessions();
-      
-      if(!isset($_SESSION['login'])){
-        $template = $this->twig->loadTemplate('login.twig');
-        echo $template->render(array('cur_user' => array('login' => ''), 'state' => ''));
-        exit;
+
+      $nbEvent = $dbEvents->getNbEventRegisteredLast($_SESSION['idutilisateur']);
+      $template = $this->twig->loadTemplate('home.twig');
+      // $sessions = $donnees->getSessionPlace();
+      $sessions = $dbSessions->getAllSessionLast();
+      for($i=0;$i<count($sessions);$i++) {
+        $nbReg = $dbEvents->getRegisteredEventPerSession($_SESSION['idutilisateur'], $sessions[$i]['idsession']);
+        $sessions[$i]['eventInscrit'] = $nbReg['count'];
       }
-      else{
-        $nbEvent = $dbEvents->getNbEventRegisteredLast($_SESSION['idutilisateur']);
-        $template = $this->twig->loadTemplate('home.twig');
-        // $sessions = $donnees->getSessionPlace();
-        $sessions = $dbSessions->getAllSessionLast();
-        for($i=0;$i<count($sessions);$i++) {
-          $nbReg = $dbEvents->getRegisteredEventPerSession($_SESSION['idutilisateur'], $sessions[$i]['idsession']);
-          $sessions[$i]['eventInscrit'] = $nbReg['count'];
-        }
-        echo $template->render(array('cur_user' => $_SESSION, 'sessions' => $sessions, 'totalRegisteredCount' => $nbEvent['count'], 'lastevent' => $dbEvents->getLastEvents($_SESSION['idutilisateur'])));
-      }
+      echo $template->render(array('cur_user' => $_SESSION, 'sessions' => $sessions, 'totalRegisteredCount' => $nbEvent['count'], 'lastevent' => $dbEvents->getLastEvents($_SESSION['idutilisateur'])));
     }
   }
 ?>
