2d93b8818c3a6460f80c925a2e9ea1215977808a
Julien Brun <jb007270@hotmail.fr>
2013-01-26 15:32:36 +0100

    Verification formulaire inscription(2) #81

---
+++ app/js/verifForm.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+var checkChamps = function(reg, e){
+  var pass = true;
+    for(champ in reg){
+      var curDiv = $('#'+champ);
+      if(!reg[champ].test($('input',curDiv).val())){ // Si le champ ne respecte pas la RegExp
+        $(curDiv).addClass('error');
+        pass = false;
+      }
+      else if($(curDiv).hasClass('error')){
+        $(curDiv).removeClass('error');
+      }
+    }
+
+    if(!pass){
+      alert('Merci de remplire tous les champs');
+      e.preventDefault();
+    }
+};
+
+  /*Verif form inscription*/
+  var inscriptionAttendu = {
+    'ctrlNom': /\w+/,
+    'ctrlPrenom': /\w+/,
+    'ctrlEmail': /^[\w\.]+@[\w\.\-]+\.\w+$/,
+    'ctrlPassword': /^.{6,}$/,
+    'ctrlPassword2': /^.{6,}$/
+  };
+
+  $("#signinform").submit(function(e){
+    checkChamps(inscriptionAttendu, e);
+  });
+
+
+});
