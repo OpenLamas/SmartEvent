4d51f45550bde3fdb23939c8ebfe570c51e2a1db
Maxime Mouchet <mouchet.max@gmail.com>
2013-01-31 15:13:41 +0100

    Patch ShowConfig

--- app/controller/admin/class_ShowConfig.php
+++ app/controller/admin/class_ShowConfig.php
@@ -1,8 +1,7 @@
 <?php
-  require('dao/class_db_request.php');
-  require('basicClass/twigStart.php');
-  require('helpers/helpers.php');
-  require('helpers/configHelper.php');
+  require_once('basicClass/twigStart.php');
+  require_once('helpers/helpers.php');
+  require_once('helpers/configHelper.php');
 
   class ShowConfig extends Controller{
 
@@ -11,16 +10,15 @@
       $this->redirectIfNotLogged();
       redirectIfHasNotTheRight('ADMIN');
 
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-          if(isset($_POST['domains']) && isset($_POST['site_root']) && isset($_POST['IPSqlServer']) && isset($_POST['PortSqlServer']) && isset($_POST['user']) && isset($_POST['dbpassword']) && isset($_POST['dbname'])){
-            if(!empty($_POST['domains']) && !empty($_POST['site_root']) && !empty($_POST['IPSqlServer']) && !empty($_POST['PortSqlServer']) && !empty($_POST['user']) && !empty($_POST['dbpassword']) && !empty($_POST['dbname'])){
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(theseKeysExistsAndAreNotEmpty($_POST, array('domains', 'site_root', 'IPSqlServer', 'PortSqlServer', 'user', 'dbpassword', 'dbname'))) {
 
-              /*if(serialize(explode(';', $_POST['domains'])) != DOMAINS || $_POST['site_root'] != SITEROOT){
+              if(serialize(explode(';', $_POST['domains'])) != DOMAINS || $_POST['site_root'] != SITEROOT){
                 $config = array();
-                $config['DOMAINS']      = 'serialize( array('.implode(',', explode(';', $_POST['domains'])).'))';
+                $config['DOMAINS']      = addslashes(serialize((explode(';', $_POST['domains']))));
                 $config['SITEROOT']     = $_POST['site_root'];
                 writeConfArrayToFile($config, 'config/config.php');
-              }*/
+              }
 
               if(HOSTNAME != $_POST['IPSqlServer'] || PORT != $_POST['PortSqlServer'] || DBUSER != $_POST['user'] || DBPASSWORD != $_POST['dbpassword'] || DBNAME != $_POST['dbname']){
                 $configDB['HOSTNAME']   = $_POST['IPSqlServer'];
@@ -33,10 +31,9 @@
               }
             }
           }
-        }
 
         $template = $this->twig->loadTemplate('config.twig');
-        echo $template->render(array('cur_user' => $_SESSION, 'hostname' => HOSTNAME, 'port' => PORT, 'dbname' => DBNAME, 'user' => DBUSER, 'password' => DBPASSWORD, 'domains' => implode(';', unserialize(DOMAINS))));
+        echo $template->render(array('cur_user' => $_SESSION, 'hostname' => HOSTNAME, 'port' => PORT, 'dbname' => DBNAME, 'user' => DBUSER, 'password' => DBPASSWORD, 'domains' => implode(';', unserialize(stripslashes(DOMAINS)))));
         exit;
     }
   }
